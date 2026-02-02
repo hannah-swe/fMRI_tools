2ae9ae016f2c0b47f52954b8a7c54d591e279e98
hannahschewe <hannah.schewe@gmail.com>
2026-02-02 13:46:11 +0100

    worked on plots

--- pipeline_comparison.py
+++ pipeline_comparison.py
@@ -8,17 +8,19 @@ from scipy.stats import pearsonr
 import statsmodels.formula.api as smf
 
 # Config / Paths
-base_dir = Path("/data_wgs04/ag-sensomotorik/PPPD/workdir_precuneus/derivatives/halfpipe")
+base_dir = Path("/data_wgs04/ag-sensomotorik/PPPD/analysis/part2_pre/derivatives/halfpipe")
 
 pipeline_patterns = {
-    "noscrub": "setting-image",
+    "no_scrub": "setting-imageNoscrub",
     "scrub": "setting-imageScrubSetting",
 }
+palette = {"no_scrub": "darkred", "scrub": "navy"}
 
-participants_path = Path("/data_wgs04/ag-sensomotorik/PPPD/BIDS_run1/participants.tsv")
-reportvals_path = Path("/data_wgs04/ag-sensomotorik/PPPD/workdir_precuneus/reports/reportvals.txt")
+participants_path = Path("/data_wgs04/ag-sensomotorik/PPPD/data/part2_pre/participants.tsv")
+reportvals_path = Path("/data_wgs04/ag-sensomotorik/PPPD/analysis/part2_pre/reports/reportvals.txt")
+subs_to_drop = ["sub-118", "sub-124", "sub-126", "sub-164"]
 
-# DMN mask must be in same grid as HALFpipe statmaps (you already resampled it)
+# DMN mask (same space as HALFpipe statmaps)
 dmn_mask = Path("/data_wgs04/ag-sensomotorik/PPPD/masks/dmn_mask_resampled.nii.gz")
 
 
@@ -65,15 +67,16 @@ def extract_qc_metrics(sub, run, pipeline, confounds_tsv, tsnr_map, brain_mask=N
 def statmap_path(sub_dir, sub, variant):
     """Paths to seed-based outputs."""
     func_task = sub_dir / "func" / "task-rest"
-    if variant == "noscrub":
-        return func_task / f"{sub}_task-rest_run-01_feature-seedbasedPrec_seed-Precuneus_stat-effect_statmap.nii.gz"
+    if variant == "no_scrub":
+        return func_task / f"{sub}_task-rest_run-01_feature-seedPrecNoscrub_seed-Precuneus_stat-effect_statmap.nii.gz"
     else:
-        return func_task / f"{sub}_task-rest_run-01_feature-seedbasedPrecScrub_seed-Precuneus_stat-effect_statmap.nii.gz"
+        return func_task / f"{sub}_task-rest_run-01_feature-seedPrecScrub_seed-Precuneus_stat-effect_statmap.nii.gz"
 
 
 
 # Load participants.tsv (group labels)
 participants_df = pd.read_csv(participants_path, sep="\t")
+participants_df["participant_id"] = "sub-" + participants_df["participant_id"].astype(str)
 
 # Load HALFpipe reportvals.txt (dashboard-consistent QC)
 reportvals_df = pd.read_fwf(reportvals_path)
@@ -147,7 +150,7 @@ if not dmn_mask.exists():
 
 for sub_dir in sorted(base_dir.glob("sub-*")):
     sub = sub_dir.name
-    for variant in ["noscrub", "scrub"]:
+    for variant in ["no_scrub", "scrub"]:
         p = statmap_path(sub_dir, sub, variant)
         if not p.exists():
             # It's fine to skip missing maps (e.g., subject failed for a feature)
@@ -163,6 +166,7 @@ for sub_dir in sorted(base_dir.glob("sub-*")):
 fc_df = pd.DataFrame(fc_rows)
 
 qc_df = qc_df.merge(fc_df, on=["sub", "pipeline"], how="left")
+qc_df = qc_df[~qc_df["sub"].isin(subs_to_drop)]
 
 
 # Optional quick sanity checks
@@ -190,9 +194,10 @@ def plot_fd_over_tr(confounds_tsv, sub, fd_threshold=None):
     sns.despine()
 
     if fd_threshold is not None:
-        plt.axhline(fd_threshold, color="red", linestyle="--", linewidth=1,
-                    label=f"FD threshold = {fd_threshold}")
-        plt.legend()
+        plt.axhline(fd_threshold, color="red", linestyle="--", linewidth=1)
+
+    ymax = max(10, fd.max())
+    plt.ylim(0, ymax)
 
     plt.tight_layout()
     plt.show()
@@ -201,31 +206,46 @@ for sub_dir in sorted(base_dir.glob("sub-*")):
     sub = sub_dir.name
     func_dir = sub_dir / "func"
 
-    confounds_tsv = func_dir / f"{sub}_task-rest_run-01_setting-image_desc-confounds_regressors.tsv"
+    confounds_tsv = func_dir / f"{sub}_task-rest_run-01_setting-imageNoscrub_desc-confounds_regressors.tsv"
     if not confounds_tsv.exists():
         continue
 
     plot_fd_over_tr(
         confounds_tsv=confounds_tsv,
         sub=sub,
-        fd_threshold=None
+        fd_threshold=5
     )
 
 # 2. percentage of scrubbed volumes on mean FD (only scrub pipeline)
-sns.scatterplot(
-    data=qc_df[qc_df.pipeline == "scrub"],
-    x="mean_fd",
-    y="pct_scrub"
+df_no = qc_df.loc[
+    qc_df["pipeline"] == "no_scrub",
+    ["sub", "mean_fd"]
+].rename(columns={"mean_fd": "mean_fd_no_scrub"})
+
+df_scrub = qc_df.loc[
+    qc_df["pipeline"] == "scrub",
+    ["sub", "pct_scrub"]
+].rename(columns={"pct_scrub": "pct_scrub_scrub"})
+
+plot_df = df_no.merge(df_scrub, on="sub", how="inner")
+
+sns.regplot(
+    data=plot_df,
+    x="mean_fd_no_scrub",
+    y="pct_scrub_scrub",
+    color="black",
 )
-plt.xlabel("Mean FD")
-plt.ylabel("% scrubbed volumes")
+plt.xlabel("Mean FD (no_scrub)")
+plt.ylabel("% scrubbed volumes (scrub)")
 sns.despine()
+plt.tight_layout()
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/scrubbed_volumes_on_mean_fd.png")
 plt.show()
 
 # 3. QC-FC plot: correlation of subject-wise motion and pipeline-wise functional connectivity in dmn
 # quick stats
 # pearson's r
-for pipeline in ["noscrub", "scrub"]:
+for pipeline in ["no_scrub", "scrub"]:
     dfp = qc_df.query("pipeline == @pipeline").dropna(
         subset=["fc_dmn_mean", "fd_mean_halfpipe"]
     )
@@ -239,27 +259,29 @@ model = smf.ols(
 ).fit()
 print(model.summary())
 
-plt.figure(figsize=(8, 6))
+plt.figure(figsize=(10, 8))
 g = sns.lmplot(
     data=qc_df,
     x="fd_mean_halfpipe",
     y="fc_dmn_mean",
     col="pipeline",
-    hue="group",
-    # aspect=1,
-    # scatter_kws={"alpha": 0.7, "s": 50},
-    # line_kws={"linewidth": 2},
+    hue="pipeline",
+    ci=95,
     legend=False,
-    ci=None
+    palette=palette
 )
 g.set_axis_labels("Mean FD", "DMN connectivity")
+plt.tight_layout()
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/fc_dmn_on_mean_fd.png")
 plt.show()
 
 # 4. Mean FD in different pipelines
 sns.boxplot(
     data=qc_df,
     x="pipeline",
-    y="mean_fd"
+    y="mean_fd",
+    palette=palette,
+    hue="pipeline"
 )
 sns.lineplot(
     data=qc_df,
@@ -268,14 +290,15 @@ sns.lineplot(
     units="sub",
     estimator=None,
     legend=False,
-    color="dimgrey",
-    linestyle="--",
-    linewidth=1,
-    alpha=1
+    color="black",
+    linewidth=1.2,
+    alpha=0.4
 )
 sns.despine()
+plt.tight_layout()
 plt.xlabel("")
 plt.ylabel("Mean Framewise Displacement")
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/mean_fd_on_pipelines.png")
 plt.show()
 
 # 4. Lineplot
@@ -288,4 +311,56 @@ sns.lineplot(
 )
 sns.despine()
 plt.ylabel("Remaining TRs")
+plt.tight_layout()
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/scrubbed_volumes_on_pipelines.png")
+plt.show()
+
+# 5. Halfpipe mean fd and other mean fd
+sns.lmplot(
+    data=qc_df[qc_df.pipeline == "no_scrub"],
+    x="mean_fd",
+    y="fd_mean_halfpipe",
+    palette=palette,
+    hue="pipeline",
+    legend=False,
+    ci=95,
+)
+plt.xlabel("Mean FD confounds")
+plt.ylabel("Mean FD reports")
+sns.despine()
+plt.tight_layout()
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/different_mean_fds.png")
+plt.show()
+
+# 6. FD difference between no scrubbing and scrubbing
+diff_df = (
+    qc_df
+    .pivot(index="sub", columns="pipeline", values="mean_fd")
+    .dropna(subset=["scrub", "no_scrub"])
+)
+
+diff_df["mean_fd_diff"] = diff_df["scrub"] - diff_df["no_scrub"]
+diff_df = diff_df.reset_index()
+
+plt.figure(figsize=(4, 6))
+sns.boxplot(
+    data=diff_df,
+    y="mean_fd_diff",
+    color="lightgray",
+    width=0.4,
+    fliersize=0
+)
+sns.stripplot(
+    data=diff_df,
+    y="mean_fd_diff",
+    color="black",
+    size=5,
+    jitter=True,
+    alpha=0.8
+)
+plt.axhline(0, color="black", linewidth=1)
+plt.ylabel("FD mean (scrub − no_scrub)")
+sns.despine()
+plt.tight_layout()
+plt.savefig("/home/hannahschewe/Documents/PPPD_analysis/quality_check_plots/mean_fd_difference.png")
 plt.show()
