da665a59ecb9e3ad392c84b5ba393b082be8dc29
hannahschewe <hannah.schewe@gmail.com>
2026-04-29 15:10:52 +0200

    work in progress

--- PPPD/group_stat_mixed_anova.py
+++ PPPD/group_stat_mixed_anova.py
@@ -19,9 +19,9 @@ import tempfile
 # ---Script configuration:
 task = "rest"
 runs = ["run-01", "run-02"] # pre, post
-part = 2 # supported: None, 1, 2 (None: all subjects; part 1: subjects < 100; part 2: subjects >= 100)
+part = None # supported: None, 1, 2 (None: all subjects; part 1: subjects < 100; part 2: subjects >= 100)
 feature = "seed_based" # supported features: "falff", "seed_based", "alff"
-seed = "InsulaOP3RAnat" # List of supported seeds:
+seed = "InsulaId1L" # List of supported seeds:
                                     # "InsulaId1L", "InsulaId1R", "InsulaIg1L", "InsulaIg1R", "InsulaIg2L", "InsulaIg2R",
                                     # "InsulaOP3RAnat", "InsulaOP3Sphere",
                                     # "IPLPFcmL", "IPLPFcmR", "IPLPFL", "IPLPFR",
@@ -32,6 +32,7 @@ group_comparison = "pat>HC" # supported comparisons: "pat>HC", "HC>pat"
 mask_strategy = "subject_based" # supported strategies: "subject_based", "predefined"
 predefined_mask = "vvn" # supported masks: "dmn", "vvn"
 threshold_mask = 0.8 # only used if mask_strategy == "subject_based"
+# number of permutations for non-parametric cluster-based permutation test
 n_perm = 5000
 
 
@@ -48,6 +49,24 @@ deriv_dir = _get_derivatives_path(feature)
 
 # get output path
 output_dir = _get_output_path(feature)
+output_dir = os.path.join(output_dir, "pre_post_diff")
+
+
+# --- Define the file suffix
+if feature == "seed_based":
+    base_title = f"{feature} {seed}; {group_comparison}"
+    file_suffix = f"{feature}_{seed}_{group_comparison}"
+else:
+    base_title = f"{feature}; {group_comparison}"
+    file_suffix = f"{feature}_{group_comparison}"
+if part is None:
+    part_label = "all"
+    base_title = f"{base_title}; subjects: {part_label}"
+else:
+    part_label = f"{part}"
+    base_title = f"{base_title}; subjects part: {part_label}"
+file_suffix = f"{file_suffix}_{part_label}"
+
 
 # --- Group mapping for contrast via predefined comparison strategy:
 if group_comparison == "pat>HC":
@@ -71,6 +90,7 @@ elif part == 2:
     selected_subs = [s for s in subs if s >= 100]
 else:
     raise ValueError("part must be None, 1, or 2")
+# exclude subjects
 selected_subs = [s for s in selected_subs if s not in subjects_to_exclude]
 print(f"Selected part: {part if part is not None else 'all'}")
 print(f"Selected subjects before loading: {len(selected_subs)}")
@@ -82,31 +102,19 @@ diff_imgs = []
 included_rows = []
 sub_mask_imgs = []
 
-diff_output_dir = os.path.join(output_dir, "pre_post_diff")
-os.makedirs(diff_output_dir, exist_ok=True)
-
 for s in selected_subs:
-
+    # get full subject id
     subject_id = f"sub-{s:03d}"
 
+    # load statistical nifti images of both runs
     run_imgs = {}
     run_masks = {}
-
     for run in runs:
-
         filename = _get_full_filename(subject_id, task, run, feature, seed)
-        img_path = os.path.join(
-            deriv_dir,
-            subject_id,
-            "func",
-            f"task-{task}",
-            filename
-        )
-
+        img_path = os.path.join(deriv_dir, subject_id, "func", f"task-{task}", filename)
         if not os.path.exists(img_path):
             print(f"Missing file: {img_path}")
             continue
-
         try:
             nib.load(img_path)
             run_imgs[run] = img_path
@@ -114,20 +122,13 @@ for s in selected_subs:
             print(f"Error loading {img_path}: {e}")
             continue
 
+        # load subject masks only if subject-based mask strategy is used
         if mask_strategy == "subject_based":
             mask_filename = _get_mask_filename(subject_id, task, run, feature, seed)
-            mask_path = os.path.join(
-                deriv_dir,
-                subject_id,
-                "func",
-                f"task-{task}",
-                mask_filename
-            )
-
+            mask_path = os.path.join(deriv_dir, subject_id, "func", f"task-{task}", mask_filename)
             if not os.path.exists(mask_path):
                 print(f"Missing mask: {mask_path}")
                 continue
-
             try:
                 nib.load(mask_path)
                 run_masks[run] = mask_path
@@ -135,7 +136,7 @@ for s in selected_subs:
                 print(f"Error loading mask {mask_path}: {e}")
                 continue
 
-    # require both runs
+    # make sure all subjects do have images for both runs
     if "run-01" not in run_imgs or "run-02" not in run_imgs:
         print(f"Skipping {subject_id}: incomplete pre/post images")
         continue
@@ -146,18 +147,10 @@ for s in selected_subs:
             print(f"Skipping {subject_id}: incomplete pre/post masks")
             continue
 
-    diff_img = math_img(
-        "post - pre",
-        post=run_imgs["run-02"],
-        pre=run_imgs["run-01"]
-    )
-
-    if feature == "seed_based":
-        diff_filename = f"{subject_id}_task-{task}_{feature}_{seed}_post-minus-pre.nii.gz"
-    else:
-        diff_filename = f"{subject_id}_task-{task}_{feature}_post-minus-pre.nii.gz"
-
-    diff_path = os.path.join(diff_output_dir, diff_filename)
+    # compute difference image between post/run-02 and pre/run-01
+    diff_img = math_img("post - pre", post=run_imgs["run-02"], pre=run_imgs["run-01"])
+    diff_filename = f"{subject_id}_task-{file_suffix}_post-minus-pre.nii.gz"
+    diff_path = os.path.join(output_dir, "diff_images", diff_filename)
     diff_img.to_filename(diff_path)
 
     diff_imgs.append(diff_path)
@@ -177,7 +170,6 @@ for s in selected_subs:
         ])
 
 included_df = pd.DataFrame(included_rows)
-
 print("Loaded difference images:", len(diff_imgs))
 print("Loaded subjects:", included_df["subject_id"].nunique())
 
@@ -200,54 +192,45 @@ else:
     raise ValueError(f"Unknown mask strategy {mask_strategy}")
 
 
-# --- Get design matrix for pre-post / group model
-design_df = included_df.merge(
-    participants_df[["subject_id", "group"]],
-    on="subject_id",
-    how="left"
-)
-
+# --- Get design matrix for pre-post model and plot it
+design_df = included_df.merge(participants_df[["subject_id", "group"]], on="subject_id", how="left")
 design_df["group_code"] = design_df["group"].map(group_mapping)
-
 if design_df["group_code"].isna().any():
     print(design_df[design_df["group_code"].isna()][["subject_id", "group"]])
     raise ValueError("Some subjects have missing or unmapped group labels.")
-
 # keep exact image order
 diff_imgs = design_df["diff_img"].tolist()
-
 second_level_design = pd.DataFrame({
     "intercept": np.ones(len(design_df)),
     "group": design_df["group_code"].astype(float)
 })
-
 print("Number of difference images:", len(diff_imgs))
 print("Design matrix shape:", second_level_design.shape)
+# plot_design_matrix(second_level_design)
+# plt.show()
 
-plot_design_matrix(second_level_design)
-plt.show()
 
+# --- PARAMETRIC TEST (voxel-wise two sample t-test unpaired)
 # fit model (here: z-scores are used, also possible: 'z_score', 'stat', 'p_value', 'effect_size', 'effect_variance', 'all')
 second_level_model = SecondLevelModel(mask_img=analysis_mask)
 second_level_model = second_level_model.fit(diff_imgs, design_matrix=second_level_design)
 z_map = second_level_model.compute_contrast("group", output_type='z_score')
 
-
-# --- PARAMETRIC TESTS with different versions of threshold and correction for multiple comparisons:
+# Significance test:
 # Version 1: abs(z) > 3.09 (equivalent to p < 0.001 one-sided test), cluster size > 10 voxels
 # z(threshold)=3.09 for p=0.001 when testing one-sided; 3.29 for two-sided
 thresholded_map1 = threshold_img(z_map, cluster_threshold=10, threshold=3.09, two_sided=True)
 thr1_data = thresholded_map1.get_fdata()
 # plot thresholded maps if there are any voxels/clusters left
 if np.any(thr1_data != 0):
-    plot_stat_map(thresholded_map1, display_mode='mosaic', cmap="inferno"
-                  ) #title=f"z map {base_title}; z > 3.09; clusters > 10 voxels"
+    plot_stat_map(thresholded_map1, display_mode='mosaic', cmap="RdBu_r",
+                  title=f"difference z map (post - pre) \n {base_title}; z > 3.09; clusters > 10 voxels")
     plt.show()
     fig = plt.figure(figsize=(9,5))
     display = plot_glass_brain(thresholded_map1, cmap="RdBu_r",
                                figure=fig, title=None, plot_abs=False, symmetric_cbar=True)
-    display.frame_axes.figure.suptitle(f"z map; z > 3.09; clusters > 10 voxels")
-    # display.savefig(os.path.join(output_dir, "01_uncorrected", f"{file_suffix}_uncorrected_p001_cluster10.png"))
+    display.frame_axes.figure.suptitle(f"difference z map (post - pre) \n {base_title}; z > 3.09; clusters > 10 voxels")
+    display.savefig(os.path.join(output_dir, "01_uncorrected", f"{file_suffix}_uncorrected_p001_cluster10.png"))
 else:
     print("No suprathreshold clusters; skipping plots.")
 # get cluster table with anatomical labels
@@ -278,24 +261,47 @@ with warnings.catch_warnings():
         verbose=1,
     )
 
-# Convert corrected -log10(p) maps into thresholded views (corrected p < .05  <=>  -log10(p) > 1.30103)
+# convert corrected -log10(p) maps into thresholded views (corrected p < .05  <=>  -log10(p) > 1.30103)
 neglog_alpha_05 = -np.log10(0.05)
-logp_size_thr = threshold_img(perm_out["logp_max_size"], threshold=neglog_alpha_05, two_sided=True)
-logp_mass_thr = threshold_img(perm_out["logp_max_mass"], threshold=neglog_alpha_05, two_sided=True)
-logp_voxel_thr = threshold_img(perm_out["logp_max_t"], threshold=neglog_alpha_05, two_sided=True)
+# use parametric z-map only for the sign/direction
+sign_z_map = second_level_model.compute_contrast("group", output_type="z_score")
+# signed cluster-mass corrected -log10(p) map
+signed_logp_mass_thr = math_img(
+    f"np.where(logp > {neglog_alpha_05}, np.sign(z) * logp, 0)",
+    logp=perm_out["logp_max_mass"],
+    z=sign_z_map,
+)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    logp_size_thr = threshold_img(perm_out["logp_max_size"], threshold=neglog_alpha_05, two_sided=True)
+    logp_mass_thr = threshold_img(perm_out["logp_max_mass"], threshold=neglog_alpha_05, two_sided=True)
+    logp_voxel_thr = threshold_img(perm_out["logp_max_t"], threshold=neglog_alpha_05, two_sided=True)
 
-# --- Plot cluster-mass corrected map
-if np.any(logp_mass_thr.get_fdata() != 0):
-    data = logp_mass_thr.get_fdata()
-    visible = data[data > neglog_alpha_05]
-    if len(visible) > 0:
-        vmax = np.ceil(np.max(visible) * 10) / 10
+# plot cluster-mass corrected map
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    if np.any(logp_mass_thr.get_fdata() != 0):
+        data = logp_mass_thr.get_fdata()
+        visible = data[data > neglog_alpha_05]
+        if len(visible) > 0:
+            vmax = np.ceil(np.max(visible) * 10) / 10
+        else:
+            vmax = neglog_alpha_05 + 0.1
+        fig = plt.figure(figsize=(9, 5))
+        display = plot_glass_brain(logp_mass_thr, cmap="inferno", threshold=neglog_alpha_05, vmin=neglog_alpha_05,
+                                   vmax=vmax, figure=fig, title=None, colorbar=True)
+        display.frame_axes.figure.suptitle(f"difference map permutation test cluster-mass FWER \n {base_title} | corrected p < .05")
+        display.savefig(os.path.join(output_dir, "04_nonparametric", f"{file_suffix}_perm_clustermass_fwer05.png"))
     else:
-        vmax = neglog_alpha_05 + 0.1
-    fig = plt.figure(figsize=(9, 5))
-    display = plot_glass_brain(logp_mass_thr, cmap="inferno", threshold=neglog_alpha_05, vmin=neglog_alpha_05, vmax=vmax,
-                               figure=fig, title=None, colorbar=True)
-    # display.frame_axes.figure.suptitle(f"Permutation test cluster-mass FWER \n {base_title} | corrected p < .05")
-    # display.savefig(os.path.join(output_dir, "04_nonparametric", f"{file_suffix}_perm_clustermass_fwer05.png"))
-else:
-    print("No clusters survive permutation cluster-mass FWER correction.")
+        print("No clusters survive permutation cluster-mass FWER correction.")
+
+# Get cluster table with aal brain atlas
+logp_mass_thr_float = math_img("img.astype(float)", img=logp_mass_thr)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    cluster_table_perm_mass = _get_cluster_table_with_aal_labels(
+        stat_img=logp_mass_thr_float,
+        stat_threshold=neglog_alpha_05,
+        cluster_threshold=0,
+        two_sided=False,
+    )
