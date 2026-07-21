299a95e11a9db5dde871b779c29e70f62d29b2d7
hannahschewe <hannah.schewe@gmail.com>
2026-07-21 14:41:52 +0200

    new colorbar without split

--- PPPD/get_cmap_svg.py
+++ PPPD/get_cmap_svg.py
@@ -4,6 +4,8 @@ import matplotlib as mpl
 from pathlib import Path
 
 outdir = Path("/home/hannahschewe/")
+
+# --- Get split RdBu cmap
 vmin = 0.7
 vmax = 2.1
 
@@ -45,4 +47,27 @@ for ax, cmap in zip(axes, [blue_half, red_half]):
 fig.savefig(outdir / "RdBu_split_colorbars.svg", bbox_inches="tight")
 fig.savefig(outdir / "RdBu_split_colorbars.pdf", bbox_inches="tight")
 
+plt.show()
+
+
+# --- Get full cmap
+vmin_full = -1
+vmax_full = 1
+
+fig, ax = plt.subplots(figsize=(6, 0.6), constrained_layout=True)
+
+norm = mpl.colors.Normalize(vmin=vmin_full, vmax=vmax_full)
+ticks = np.linspace(-1, 1, 9)
+
+cb = mpl.colorbar.ColorbarBase(
+    ax=ax,
+    cmap=mpl.cm.RdBu_r,
+    norm=norm,
+    orientation="horizontal",
+    ticks=ticks,
+)
+
+cb.ax.tick_params(labelsize=10, length=4, width=0.8)
+cb.outline.set_linewidth(0.7)
+fig.savefig(outdir / "RdBu_colorbar.svg", bbox_inches="tight")
 plt.show()
